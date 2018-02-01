9108cc447bbecb67d0d9f1a88bf3a71cf24d5d74
shivahs <shivaprasad.hs@lowes.com>
2018-02-01 12:10:33 +0530

    edit main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!")
+	io.WriteString(w, "Hello World! this is from a container!")
 }
 
 func main() {
